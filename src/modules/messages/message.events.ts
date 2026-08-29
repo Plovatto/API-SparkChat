63a46a04dc9cd0d312c5ff23ca77d7f4cb3f1be6
Plovatto <pandora-blovatto@educar.rs.gov.br>
2026-08-29 17:08:02 -0300

    :sparkles: feat: add message deletion socket event

--- src/modules/messages/message.events.ts
+++ src/modules/messages/message.events.ts
@@ -5,10 +5,12 @@ export interface MessageServerToClientEvents {
   'message:mark-read-done': (payload: { roomId: string; unreadCount: number }) => void;
   'message:read-receipt': (payload: { roomId: string; userId: string }) => void;
   'messages:list': (payload: { roomId: string; messages: MessageView[] }) => void;
+  'message:deleted': (payload: { messageId: string; roomId: string }) => void;
 }
 
 export interface MessageClientToServerEvents {
   'message:send': (payload: { roomId: string; content: string }) => void;
   'message:mark-read': (payload: { roomId: string }) => void;
   'messages:get': (payload: { roomId: string }) => void;
+  'message:delete': (payload: { messageId: string }) => void;
 }
